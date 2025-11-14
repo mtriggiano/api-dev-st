5a27dca83b2d4c501e4b1a5d736fbd04e4ff7333
Miguel Triggiano <migueltriggiano@gmail.com>
2025-11-14 01:07:31 +0000

    ssl-manager.sh: escape all Nginx vars ($host, $remote_addr, $proxy_add_x_forwarded_for, $scheme, $http_upgrade) in all templates; fix proxy_set_header errors on HTTP-only and LE

--- scripts/utils/ssl-manager.sh
+++ scripts/utils/ssl-manager.sh
@@ -274,10 +274,10 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:$PORT;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_http_version 1.1;
         proxy_read_timeout 720s;
     }
@@ -352,10 +352,10 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:$PORT;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_http_version 1.1;
         proxy_read_timeout 720s;
     }
@@ -363,20 +363,20 @@ server {
     location /websocket {
         proxy_pass http://127.0.0.1:$EVENTED_PORT;
         proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection upgrade;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_read_timeout 86400;
     }
 
     location /longpolling {
         proxy_pass http://127.0.0.1:$EVENTED_PORT;
         proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection upgrade;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_read_timeout 86400;
     }
 }
